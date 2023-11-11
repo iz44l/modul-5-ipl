7801fe6db1061710b20d1c682372cc087f23bec0
DESKTOP-E1UH8SQ\Rizal Zulkipli <02rizalzulkipli@gmail.com>
2023-11-11 18:17:09 +0700

    Sebelum update

---
+++ Customer.java
@@ -0,0 +1,8 @@
+public class Customer {
+    class DtaRcrd102 {
+        private Date genymdhms;
+        private Date modymdhms;
+        private final String pszqint = "102";
+        /* ... */
+    }
+}
