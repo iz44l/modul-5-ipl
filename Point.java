b753934a8ef5d6951ca0f6fefa3acb27fa8fa435
DESKTOP-E1UH8SQ\Rizal Zulkipli <02rizalzulkipli@gmail.com>
2023-11-11 18:23:15 +0700

    Sebelum update

--- Point.java
+++ Point.java
@@ -1,13 +1,4 @@
-public interface Point {
-    double getX();
-
-    double getY();
-
-    void setCartesian(double x, double y);
-
-    double getR();
-
-    double getTheta();
-
-    void setPolar(double r, double theta);
+public class Point {
+    public double x;
+    public double y;
 }
