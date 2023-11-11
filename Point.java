f5e9e3ee34090f563a64e1a4a023d31286f248f1
DESKTOP-E1UH8SQ\Rizal Zulkipli <02rizalzulkipli@gmail.com>
2023-11-11 18:21:10 +0700

    no message

---
+++ Point.java
@@ -0,0 +1,13 @@
+public interface Point {
+    double getX();
+
+    double getY();
+
+    void setCartesian(double x, double y);
+
+    double getR();
+
+    double getTheta();
+
+    void setPolar(double r, double theta);
+}
